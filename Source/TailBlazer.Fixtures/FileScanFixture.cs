74f8d6c31b82c250e192c718d683bda9c0d3cb24
Roland Pheasant <roland_pheasant@hotmail.com>
2015-11-16 20:06:01 +0000

    Got rid of hideous Thread.Sleep from some tests - made possible by not using the file system watcher

--- Source/TailBlazer.Fixtures/FileScanFixture.cs
+++ Source/TailBlazer.Fixtures/FileScanFixture.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using FluentAssertions;
+using Microsoft.Reactive.Testing;
 using TailBlazer.Domain.FileHandling;
 using Xunit;
 
@@ -23,18 +24,19 @@ namespace TailBlazer.Fixtures
             var file = Path.GetTempFileName();
             var info = new FileInfo(file);
             int[] result=new int[0];
+            var scheduler = new TestScheduler();
 
             File.AppendAllLines(file, Enumerable.Range(1, 100).Select(i => $"{i}").ToArray());
 
-            using (info.WatchFile().ScanFile().Subscribe(x => result = x.MatchingLines.Select(l=>l.Line).ToArray()) )           {
+            using (info.WatchFile(scheduler:scheduler).ScanFile().Subscribe(x => result = x.MatchingLines.Select(l => l.Line).ToArray()))
+            {
                 result.ShouldAllBeEquivalentTo(Enumerable.Range(1, 100));
 
                 File.AppendAllLines(file, Enumerable.Range(101, 10).Select(i => $"{i}"));
-
-                Thread.Sleep(TimeSpan.FromSeconds(1));
+                scheduler.AdvanceByMilliSeconds(250);
                 File.Delete(file);
                 result.ShouldAllBeEquivalentTo(Enumerable.Range(1, 110));
-       
+
             }
         }
 
@@ -44,17 +46,17 @@ namespace TailBlazer.Fixtures
             var file = Path.GetTempFileName();
             var info = new FileInfo(file);
             int[] result = new int[0];
-
+            var scheduler = new TestScheduler();
             File.AppendAllLines(file, Enumerable.Range(1, 100).Select(i => $"{i}").ToArray());
 
             //filter by odd numbers
-            using (info.WatchFile().ScanFile(l=>int.Parse(l) % 2 == 1).Subscribe(x => result = x.MatchingLines.Select(l=>l.Line).ToArray()))
+            using (info.WatchFile(scheduler: scheduler).ScanFile(l=>int.Parse(l) % 2 == 1).Subscribe(x => result = x.MatchingLines.Select(l=>l.Line).ToArray()))
             {
                 result.ShouldAllBeEquivalentTo(Enumerable.Range(1, 100).Where(i=>i % 2 == 1));
 
                 File.AppendAllLines(file, Enumerable.Range(101, 10).Select(i => $"{i}"));
 
-                Thread.Sleep(TimeSpan.FromSeconds(1));
+                scheduler.AdvanceByMilliSeconds(250);
                 File.Delete(file);
                 result.ShouldAllBeEquivalentTo(Enumerable.Range(1, 110).Where(i => i % 2 == 1));
             }
