c37e4e65c911ed72f612114bb88add8404a7c37b
Roland Pheasant <roland_pheasant@hotmail.com>
2015-11-28 10:05:28 +0000

    Missing file

---
+++ Source/TailBlazer/Infrastucture/ILogFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+using TailBlazer.Domain.Infrastructure;
+
+namespace TailBlazer.Infrastucture
+{
+    public interface ILogFactory
+    {
+        ILogger Create(string name);
+        ILogger Create<T>();
+    }
+}
