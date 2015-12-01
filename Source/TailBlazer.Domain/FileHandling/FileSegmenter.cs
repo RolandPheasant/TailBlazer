b63cd3e611c1615576e9a244521de16427fb3796
Roland Pheasant <roland@dynamic-data.org>
2015-12-01 07:42:56 +0000

    File segmenter

--- Source/TailBlazer.Domain/FileHandling/FileSegmenter.cs
+++ Source/TailBlazer.Domain/FileHandling/FileSegmenter.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Drawing.Printing;
+using System.Reactive.Disposables;
+using DynamicData;
+
 namespace TailBlazer.Domain.FileHandling
 {
 
@@ -10,26 +15,42 @@ namespace TailBlazer.Domain.FileHandling
 
     public class FileSegment
     {
+        public int Index { get; }
         public int Start { get;  }
         public int End { get;  }
         public FileSegmentType Type { get;  }
 
-        public FileSegment(int start, int end, FileSegmentType type)
+        public FileSegment(int index, int start, int end, FileSegmentType type)
         {
+            Index = index;
             Start = start;
             End = end;
             Type = type;
         }
     }
 
-    public class FileSegmenter
+    public sealed class FileSegmenter: IDisposable
     {
-        //dynamically split the file into sements according to the size of file.
+        //dynamically split the file into segments according to the size of file.
         //as the file size changes, allow these segments to dynamically resize
 
+       //additionally seperately  monitor the head of the file??
+       
+       //this is very useful for parallelising searches searches.
+       private readonly IObservableCache<FileSegment,int> _cache = new SourceCache<FileSegment, int>(fs=>fs.Index);
 
-        public FileSegmenter()
+        private IDisposable _cleanup;
+
+        public FileSegmenter(int endSize= 1000000, int resizeFactor=10)
         {
+            //calculate 
+
+            _cleanup = new CompositeDisposable(_cache);
+        }
+
+        public void Dispose()
+        {
+
         }
     }
 }
