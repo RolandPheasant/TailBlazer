8f5d0820331934e6fb984178ece3ca5744a67b52
Kristian Hellang <kristian@hellang.com>
2015-12-14 16:03:55 +0100

    Added DistinctPathTrie

---
+++ Source/TailBlazer.Fixtures/DistinctPathTrieFixture.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using TailBlazer.Infrastucture;
+using Xunit;
+using static System.IO.Path;
+
+namespace TailBlazer.Fixtures
+{
+    public class DistinctPathTrieFixture
+    {
+        [Fact]
+        public void ReturnsCorrectDistinctPath()
+        {
+            var paths = new[]
+            {
+                Combine("logger.log"),
+                Combine("Debug", "logger.log"),
+                Combine("bin", "Debug", "logger.log"),
+                Combine("C:", "App", "bin", "Debug", "logger.log"),
+                Combine("D:", "App", "bin", "Debug", "logger.log"),
+                Combine("C:", "App", "bin", "Release", "logger.log"),
+                Combine("C:", "App", "obj", "Release", "logger.log")
+            };
+
+            var expected = new[]
+            {
+                Combine("logger.log"),
+                Combine("Debug", "logger.log"),
+                Combine("bin", "Debug", "logger.log"),
+                Combine("C:", "App", "bin", "Debug", "logger.log"),
+                Combine("D:", "App", "bin", "Debug", "logger.log"),
+                Combine("bin", "Release", "logger.log"),
+                Combine("obj", "Release", "logger.log")
+            };
+
+            var trie = new DistinctPathTrie(paths);
+
+            var result = paths.Select(path => trie.GetDistinctPath(path)).ToArray();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Source/TailBlazer/Infrastucture/DistinctPathTrie.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TailBlazer.Infrastucture
+{
+    public class DistinctPathTrie
+    {
+        private static readonly char[] DirectorySeparators =
+        {
+            Path.DirectorySeparatorChar,
+            Path.AltDirectorySeparatorChar
+        };
+
+        public DistinctPathTrie(IEnumerable<string> paths)
+        {
+            foreach (var path in paths)
+            {
+                Insert(path);
+            }
+        }
+
+        private Node Root { get; } = new Node(string.Empty);
+
+        public string GetDistinctPath(string path)
+        {
+            var result = GetDistinctPath(Root, new Stack<string>(path.Split(DirectorySeparators)));
+
+            return string.Join(Path.DirectorySeparatorChar.ToString(), result);
+        }
+
+        private static Stack<string> GetDistinctPath(Node node, Stack<string> path)
+        {
+            return GetDistinctPath(node, path, result: new Stack<string>());
+        }
+
+        private static Stack<string> GetDistinctPath(Node node, Stack<string> path, Stack<string> result)
+        {
+            if (!path.Any())
+            {
+                return result;
+            }
+
+            if (result == null)
+            {
+                result = new Stack<string>();
+            }
+
+            var part = path.Pop();
+
+            result.Push(part);
+
+            Node childNode;
+            if (node.Children.TryGetValue(part, out childNode) && childNode.Count > 1)
+            {
+                return GetDistinctPath(childNode, path, result);
+            }
+
+            return result;
+        }
+
+        public void Insert(string path)
+        {
+            Insert(Root, path: new Stack<string>(path.Split(DirectorySeparators)));
+        }
+
+        private static void Insert(Node node, Stack<string> path)
+        {
+            if (!path.Any())
+            {
+                return;
+            }
+
+            var part = path.Pop();
+
+            Node childNode;
+            if (!node.Children.TryGetValue(part, out childNode))
+            {
+                node.Children.Add(part, childNode = new Node(part));
+            }
+
+            childNode.Count++;
+
+            Insert(childNode, path);
+        }
+
+        private class Node
+        {
+            public Node(string value)
+            {
+                Value = value;
+                Children = new Dictionary<string, Node>(StringComparer.OrdinalIgnoreCase);
+            }
+
+            private string Value { get; }
+
+            public int Count { get; set; }
+
+            public IDictionary<string, Node> Children { get; }
+
+            public override string ToString()
+            {
+                return $"Value: {Value}, Children: {Children.Count}";
+            }
+        }
+    }
+}
