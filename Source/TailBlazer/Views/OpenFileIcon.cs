a2fa2f51a3aedf6cd3be1d0611f9854e4c39e842
Roland Pheasant <roland_pheasant@hotmail.com>
2015-12-20 12:05:29 +0000

    Style tidy up

--- Source/TailBlazer/Views/OpenFileIcon.cs
+++ Source/TailBlazer/Views/OpenFileIcon.cs
@@ -15,35 +15,7 @@ using System.Windows.Shapes;
 
 namespace TailBlazer.Views
 {
-    /// <summary>
-    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-    ///
-    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:TailBlazer.Views"
-    ///
-    ///
-    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:TailBlazer.Views;assembly=TailBlazer.Views"
-    ///
-    /// You will also need to add a project reference from the project where the XAML file lives
-    /// to this project and Rebuild to avoid compilation errors:
-    ///
-    ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Browse to and select this project]
-    ///
-    ///
-    /// Step 2)
-    /// Go ahead and use your control in the XAML file.
-    ///
-    ///     <MyNamespace:OpenFileIcon/>
-    ///
-    /// </summary>
+
     public class OpenFileIcon : Control
     {
         static OpenFileIcon()
