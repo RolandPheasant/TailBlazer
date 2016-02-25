5594ec6b5fb0062e897806c201fe1e9a318793dd
Roland Pheasant <roland@dynamic-data.org>
2016-02-25 21:42:54 +0000

    Layout - missing files

---
+++ Source/TailBlazer/Views/Layout/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TailBlazer.Views.Layout
+{
+    public class Node
+    {
+    }
+
+    public class RootNode : Node
+    {
+        
+    }
+}
---
+++ Source/TailBlazer/Views/Layout/RootState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TailBlazer.Domain.Settings;
+
+namespace TailBlazer.Views.Layout
+{
+
+}
