8dc48376542305a5b943abc348445aa5676f3b76
Filip De Vos <filipdevoskls@gmail.com>
2016-05-15 10:54:59 -0400

    Add tests for IconProvider

---
+++ Source/TailBlazer.Fixtures/IconProviderFixture.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Xunit;
+using FluentAssertions;
+using TailBlazer.Views.Formatting;
+
+namespace TailBlazer.Fixtures
+{
+    public class IconProviderFixture
+    {
+        [Fact]
+        public void IconProviderShouldHaveIcons()
+        {
+            using (var provider = new IconProvider(new DefaultIconSelector()))
+            {
+                var result = provider.Icons;
+
+                result.Items.Any().Should().BeTrue();
+            }
+        }
+    }
+}
