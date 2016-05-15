655400a4d2f5872b04a0b811a3b8cbae21ab8435
Filip De Vos <filipdevoskls@gmail.com>
2016-05-15 10:47:11 -0400

    Add tests for DefaultIconSelector
    Due to adding the materialdesign library as reference some tests had to
    be changed. The icons library now gets populated which resulted in
    different output.

---
+++ Source/TailBlazer.Fixtures/IconDescriptionFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using TailBlazer.Views.Formatting;
+using MaterialDesignThemes.Wpf;
+
+namespace TailBlazer.Fixtures
+{
+    public class IconDescriptionFixture
+    {
+        [Fact]
+        public void IconDescriptionShoudBeComparable()
+        {
+            var id1 = new IconDescription(new PackIconKind(), "test");
+            var id2 = new IconDescription(new PackIconKind(), "test");
+ 
+            var result = id1 == id2;
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IconDescriptionShoudBeComparableInEqual()
+        {
+            var id1 = new IconDescription(new PackIconKind(), "test1");
+            var id2 = new IconDescription(new PackIconKind(), "test2");
+
+            var result = id1 == id2;
+
+            result.Should().BeFalse();
+        }
+
+    }
+}
